0c0a68df983a379f82e606caa96d1084d16524a2
Rodrigo Manhães <rmanhaes@gmail.com>
2011-04-26 20:20:50 -0300

    Sheldon Cooper holds a Ph.D.

--- calve_machine/spec/calve_machine_spec.py
+++ calve_machine/spec/calve_machine_spec.py
@@ -7,12 +7,12 @@ class CalveMachineSpec(unittest.TestCase):
     def it_calves_an_object_given_plain_attributes(self):
         @inseminate
         def programmer(p):
-            p.name = 'Sheldon Cooper'
+            p.name = 'Sheldon Cooper, Ph.D.'
             p.age = 29
             p.languages = ['eiffel', 'io', 'erlang']
 
         prog = pregnant.calve('programmer')
-        prog.name |should| equal_to('Sheldon Cooper')
+        prog.name |should| equal_to('Sheldon Cooper, Ph.D.')
         prog.age |should| be(29)
         prog.languages |should| equal_to(['eiffel', 'io', 'erlang'])
 
@@ -20,7 +20,7 @@ class CalveMachineSpec(unittest.TestCase):
     def it_inherits_insemination(self):
         @inseminate
         def programmer(p):
-            p.name = 'Sheldon Cooper'
+            p.name = 'Sheldon Cooper, Ph.D.'
             p.age = 29
             p.languages = ['eiffel', 'io', 'erlang']
 
@@ -30,7 +30,7 @@ class CalveMachineSpec(unittest.TestCase):
             p.foo = 'spam'
 
         prog = pregnant.calve('python_programmer')
-        prog.name |should| equal_to('Sheldon Cooper')
+        prog.name |should| equal_to('Sheldon Cooper, Ph.D.')
         prog.age |should| be(29)
         prog.languages |should| equal_to(['python'])
         prog.foo |should| equal_to('spam')
